bceda18945ad5a324a3535e97dd811479e81c7cf
João <joaovictorvha@gmail.com>
2019-08-30 14:30:28 -0400

    get user detail

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,19 @@
 const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
-const { db } = require('./util/admin');;
-const { signup, login, addUserDetails } = require('./handlers/users');
+const { db } = require('./util/admin');
+// const { signup, login, uploadImage, addUserDetails, getAuthenticadUser, getUserDetails, markNotificationsRead } = require('./handlers/users');
+const { signup, login, addUserDetails, getUserDetails  } = require('./handlers/users');
 
 // Users routes
 app.post('/signup', signup);
 app.post('/login', login);
-
+// app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.put('/user', FBAuth, addUserDetails);
+// app.get('/user', FBAuth, getAuthenticadUser);
+app.get('/user/:userId', getUserDetails);
+// app.post('/notifications', FBAuth, markNotificationsRead);
+
 // https://baseurl/api/
 exports.api = functions.https.onRequest(app);
