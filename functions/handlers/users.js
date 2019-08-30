84306e5d0247893b6a4d9f00157f42bbaa6a69a1
João <joaovictorvha@gmail.com>
2019-08-30 14:53:07 -0400

    upload image

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -160,61 +160,62 @@ exports.getUserDetails = (req, res) => {
 //     })
 // }
 
-// // Upload a profile pic
-// exports.uploadImage = (req, res) => {
-//     const BusBoy = require('busboy');
-//     const path = require('path');
-//     const os = require('os');
-//     const fs = require('fs');
+// Upload a profile pic
+exports.uploadImage = (req, res) => {
+    const BusBoy = require('busboy');
+    const path = require('path');
+    const os = require('os');
+    const fs = require('fs');
   
-//     const busboy = new BusBoy({ headers: req.headers });
+    const busboy = new BusBoy({ headers: req.headers });
   
-//     let imageToBeUploaded = {};
-//     let imageFileName;
+    let imageToBeUploaded = {};
+    let imageFileName;
   
-//     busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-//       console.log(fieldname, file, filename, encoding, mimetype);
-//       if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
-//         return res.status(400).json({ error: 'Wrong file type submitted' });
-//       }
-//       // my.image.png => ['my', 'image', 'png']
-//       const imageExtension = filename.split('.')[filename.split('.').length - 1];
-//       // 32756238461724837.png
-//       imageFileName = `${Math.round(
-//         Math.random() * 1000000000000
-//       ).toString()}.${imageExtension}`;
-//       const filepath = path.join(os.tmpdir(), imageFileName);
-//       imageToBeUploaded = { filepath, mimetype };
-//       file.pipe(fs.createWriteStream(filepath));
-//     });
-//     busboy.on('finish', () => {
-//       admin
-//         .storage()
-//         .bucket()
-//         .upload(imageToBeUploaded.filepath, {
-//           resumable: false,
-//           metadata: {
-//             metadata: {
-//               contentType: imageToBeUploaded.mimetype
-//             }
-//           }
-//         })
-//         .then(() => {
-//           const imageUrl = `https://firebasestorage.googleapis.com/v0/b/${
-//             config.storageBucket
-//           }/o/${imageFileName}?alt=media`;
-//           return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
-//         })
-//         .then(() => {
-//           return res.json({ message: 'image uploaded successfully' });
-//         })
-//         .catch((err) => {
-//           console.error(err);
-//           return res.status(500).json({ error: 'something went wrong' });
-//         });
-//     });
-//     busboy.end(req.rawBody);
-//   };
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+      console.log(fieldname, file, filename, encoding, mimetype);
+      if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
+        return res.status(400).json({ error: 'Tipo de arquivo incompatível!' });
+      }
+      // my.image.png => ['my', 'image', 'png']
+      const imageExtension = filename.split('.')[filename.split('.').length - 1];
+      // 32756238461724837.png
+      imageFileName = `${Math.round(
+        Math.random() * 1000000000000
+      ).toString()}.${imageExtension}`;
+      const filepath = path.join(os.tmpdir(), imageFileName);
+      imageToBeUploaded = { filepath, mimetype };
+      file.pipe(fs.createWriteStream(filepath));
+    });
+    busboy.on('finish', () => {
+      admin
+        .storage()
+        .bucket()
+        .upload(imageToBeUploaded.filepath, {
+          resumable: false,
+          metadata: {
+            metadata: {
+              contentType: imageToBeUploaded.mimetype
+            }
+          }
+        })
+        .then(() => {
+          const urlImagem = `https://firebasestorage.googleapis.com/v0/b/${
+            config.storageBucket
+          }/o/${imageFileName}?alt=media`;
+          const emailBase64 = new Buffer(req.user.email).toString('base64');
+          return db.doc(`/usuarios/${emailBase64}`).update({ urlImagem });
+        })
+        .then(() => {
+          return res.json({ message: 'Upload realizado com sucesso' });
+        })
+        .catch((err) => {
+          console.error(err);
+          return res.status(500).json({ error: 'Algo deu errado, tente novamente!' });
+        });
+    });
+    busboy.end(req.rawBody);
+  };
 
 //   exports.markNotificationsRead = (req, res) => {
 //       let batch = db.batch();
--- functions/index.js
+++ functions/index.js
@@ -3,12 +3,12 @@ const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 const { db } = require('./util/admin');
 // const { signup, login, uploadImage, addUserDetails, getAuthenticadUser, getUserDetails, markNotificationsRead } = require('./handlers/users');
-const { signup, login, addUserDetails, getUserDetails  } = require('./handlers/users');
+const { signup, login, uploadImage, addUserDetails, getUserDetails } = require('./handlers/users');
 
 // Users routes
 app.post('/signup', signup);
 app.post('/login', login);
-// app.post('/user/image', FBAuth, uploadImage);
+app.post('/user/image', FBAuth, uploadImage);
 app.post('/user', FBAuth, addUserDetails);
 app.put('/user', FBAuth, addUserDetails);
 // app.get('/user', FBAuth, getAuthenticadUser);
