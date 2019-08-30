5702ea12a27ff01a7aae54b311b1c49d34cb8cf1
João <joaovictorvha@gmail.com>
2019-08-30 14:24:31 -0400

    Signup

--- functions/util/fbAuth.js
+++ functions/util/fbAuth.js
@@ -5,27 +5,27 @@ module.exports = (req, res, next) => {
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
         idToken = req.headers.authorization.split('Bearer ')[1];
     } else {
-        console.error('No token found')
-        return res.status(403).json({ error: 'Unauthorized' })
+        console.error('Token não existe')
+        return res.status(403).json({ error: 'Não autorizado' })
     }
 
     admin.auth().verifyIdToken(idToken)
     .then(decodedToken => {
         req.user = decodedToken;
         console.log(decodedToken);        
-        // Get user
-        // return db.collection('users')
-        // .where('userId', '==', req.user.uid)
-        // .limit(1)
-        // .get();
+        //Get user
+        return db.collection('usuarios')
+        .where('id', '==', new Buffer(req.user.email).toString('base64'))
+        .limit(1)
+        .get();
     })
     .then(data => {
-        // req.user.handle = data.docs[0].data().handle; Get userId
-        // req.user.imageUrl = data.docs[0].data().imageUrl; Get userImage
+        req.user.id = new Buffer(data.docs[0].data().email).toString('base64');; //Get Base64 email
+        req.user.imageUrl = data.docs[0].data().imageUrl; //Get userImage
         return next();
     })
     .catch(err => {
-        console.error('Error while verifying token', err);
+        console.error('Erro durante a verificação de token', err);
         return res.status(403).json(err);
     })
 }
