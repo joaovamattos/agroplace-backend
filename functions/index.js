f1b460a1d83f1c4b5a6193d0267690c902854b5c
João <joaovictorvha@gmail.com>
2019-08-30 14:26:18 -0400

    login

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,11 @@ const functions = require('firebase-functions');
 const app = require('express')();
 const FBAuth = require('./util/fbAuth');
 const { db } = require('./util/admin');;
-const { signup } = require('./handlers/users');
+const { signup, login } = require('./handlers/users');
 
 // Users routes
 app.post('/signup', signup);
+app.post('/login', login);
 
 // https://baseurl/api/
 exports.api = functions.https.onRequest(app);
